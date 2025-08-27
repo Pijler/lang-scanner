8b71bfdc22bbe13982096cfc910610216f11630a
João Pedro Lopes <joaopalopes24@gmail.com>
2025-08-27 16:40:33 -0300

    feat(status): adjust Status enum

--- app/Enum/Status.php
+++ app/Enum/Status.php
@@ -6,6 +6,19 @@ enum Status: string
 {
     case OK = 'ok';
     case ERROR = 'error';
+    case SKIPPED = 'skipped';
+
+    /**
+     * Gets the color for the status.
+     */
+    public function color(): string
+    {
+        return match ($this) {
+            self::OK => 'green',
+            self::ERROR => 'red',
+            self::SKIPPED => 'gray',
+        };
+    }
 
     /**
      * Gets the symbol and format for the status.
@@ -13,7 +26,8 @@ enum Status: string
     public function symbol(): array
     {
         return match ($this) {
-            self::OK => ['symbol' => '.', 'format' => '<fg=gray>%s</>'],
+            self::SKIPPED => ['symbol' => '.', 'format' => '<fg=gray>%s</>'],
+            self::OK => ['symbol' => '✓', 'format' => '<options=bold;fg=green>%s</>'],
             self::ERROR => ['symbol' => '⨯', 'format' => '<options=bold;fg=red>%s</>'],
         };
     }
