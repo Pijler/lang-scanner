cf4313293b2600f8e33418652db42b924113c9b6
João Pedro Lopes <joaopalopes24@gmail.com>
2025-09-04 14:03:00 -0300

    feat(tests): adding fixtures

---
+++ tests/Fixtures/test-c/resources/app/app.blade.php
@@ -0,0 +1,24 @@
+<div>
+  {{ __('App Name') }}
+  <br />
+  {{ trans('App Version') }}
+  <br />
+  {{ trans_choice('App Description') }}
+  <br />
+  {{ __('app.name') }}
+  <br />
+  {{ trans('app.version') }}
+  <br />
+  {{ trans_choice('app.description') }}
+  <br />
+  {{ __('App. Name!') }}
+  <br />
+  {{ trans('App. Version!') }}
+  <br />
+  {{ trans_choice('App. Description!') }}
+  {{ __('App. Name.') }}
+  <br />
+  {{ trans('App. Version.') }}
+  <br />
+  {{ trans_choice('App. Description.') }}
+</div>
