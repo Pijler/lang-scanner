0b20157ab5fe75770418216af6393053a3782871
João Pedro Lopes <joaopalopes24@gmail.com>
2025-09-17 10:01:18 -0300

    ci(github): adding github workflow

--- tests/Fixtures/test-f/resources/base/base.blade.php
+++
@@ -1,7 +0,0 @@
-<div>
-  {{ __("The app's performance can't be beat.") }}
-  <br />
-  {{ trans("Don't forget to update your app.") }}
-  <br />
-  {{ trans("If you're enjoying this app, please leave a review!") }}
-</div>
